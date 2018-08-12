fcc61329c52208977e5f3f6cb35970cacb50bbf2
黄小明1314 <www.dnng@qq.com>
2018-08-13 00:20:44 +0800

    Signed-off-by: 黄小明1314 <www.dnng@qq.com>

--- Uart_V.2.0/Uart/Uart.ino
+++ Uart_V.2.0/Uart/Uart.ino
@@ -22,6 +22,8 @@ int Val_E_1 = 0;
 int LED     = 7;
 int gsm     = 0;
 int State   = 0;
+int gsm_count = 0;
+int flag = 0;
 String comdata   = " ";     //串口上位机数据接收缓存区
 char AT_CMHF[]   = "AT+CMGF=0";
 char AT_CMGS[]   = "AT+CMGS=39";
@@ -35,103 +37,94 @@ uchar Humidity[5]      = {0xff, 0x61, 0x22, 0x00, 0xff}; //湿度
 uchar Temperature[5]   = {0xff, 0x62, 0x22, 0x00, 0xff}; //温度
 uchar Concentration[5] = {0xff, 0x63, 0x22, 0x00, 0xff}; //浓度
 
-void Forward();
-void Back();
-void Stop();
-void Gsm_Pdu();
-void Serial_Receiving();
+//void Forward();
+//void Back();
+//void Stop();
+//void Gsm_Pdu();
+//void Serial_Receiving();
 
 
 void setup() {
   // put your setup code here, to run once:
-      mySerial.begin(115200);
-      Serial.begin(9600);
-      pinMode(LED, OUTPUT);
-      pinMode(DHT11PIN, OUTPUT);    //定义DHT11D的输出口
-      pinMode(9,OUTPUT);
-      pinMode(10,OUTPUT);
-      pinMode(5,OUTPUT);
-      pinMode(6,OUTPUT);
-      pinMode(Elec_2PIN, INPUT);
-      pinMode(Elec_1PIN, INPUT);
-      pinMode(GsmPIN, INPUT);
-      pinMode(Buzz, OUTPUT);
-      Init_Data();
-      MsTimer2::set(1000, flash); //中断设置函数，每1000ms进入一次中断
-      MsTimer2::start();//开始计时
+  mySerial.begin(115200);
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(DHT11PIN, OUTPUT);    //定义DHT11D的输出口
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(Elec_2PIN, INPUT);
+  pinMode(Elec_1PIN, INPUT);
+  pinMode(GsmPIN, INPUT);
+  pinMode(Buzz, OUTPUT);
+  Init_Data();
+  MsTimer2::set(1000, flash); //中断设置函数，每1000ms进入一次中断
+  MsTimer2::start();//开始计时
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //串口从上位机接收数据
-//  Stop();
-//     while (Serial.available() > 0)  
-//    {
-//        comdata += char(Serial.read()); //一位一位的读取串口数据，串接到comedata
-//        delay(2);
-//    }
-//         Stop();
-//    if (comdata.length() > 0)  //还有数据就继续发送
-//  {
-//    Serial.print("  stata  " );
-//   Serial.print(comdata[3],HEX);
-//   Serial.print("  ss  " );
-//    comdata = ""; //数据清空，否则会影响下一次的数据
-//   }
-
+  if(flag == 0)
+  {
+      Stop(20);
+     Forward_count(18);
+      Stop(1);
+      Turn_right(9);
+      Stop(1);
+      Forward_count(13);
+      Stop(100);
+      flag++;
+  }
+  
+  Serial_Receiving();
+  //前进
+  while (State == 8)
+  {
+    Forward();
     Serial_Receiving();
-      //前进
-      while(State == 8)
-      {
-       Forward();
-         Serial_Receiving();
-      }
-      //后退
-      while(State == 1)
-      {
-      Back();
-      Serial_Receiving();
-      }
-      //停止
-      while(State == 2)
-      {
-      Stop();
-      Serial_Receiving();
-      }
-      //发送PUD短信
-      if (State == 7)
-      {
-      // Back();
+  }
+  //后退
+  while (State == 1)
+  {
+    Back();
+    Serial_Receiving();
+  }
+  //停止
+  while (State == 2)
+  {
+    Stop(10);
+    Serial_Receiving();
+  }
+  // 一级报警,发送PUD短信
+
+  int Gsm_Vaule = digitalRead(GsmPIN);
+  if (Gsm_Vaule == 0)
+  {
+    if (gsm_count < 0)
+    {
       delay(1);
       Gsm_Pdu();
-      }
+      gsm_count++;
+      State = 2;
+    }
+  }
+
+  if (Val == 50 && Temp == 60)
+  {
+    GPRS_Call();
+  }
 
   //刷新传感器数据
   Dht11();
   MQ_2();
   Electricity_Display();
   Electricity_Display_1();
-//
-//  //一级报警
-//  int Gsm_Vaule = digitalRead(GsmPIN);
-////  int Gsm_Vaule_1 = !Gsm_Vaule;
-//  Serial.print("  GSM   ");
-//  Serial.print(Gsm_Vaule);
-//  Serial.print("  OVER  ");
-////  Serial.print("  GSM_1   ");
-////  Serial.print(Gsm_Vaule_1);
-////  Serial.print("  OVER  ");
-//if(millis()>60000)
-// {  if (Gsm_Vaule == 0)
-//  { //Gsm_Pdu_Yiji();
-////    Forward();
-//  digitalWrite(Buzz, HIGH);
-//  delay(100);
-//  digitalWrite(Buzz,  LOW);
-//  }
-// }
-//
+
+
+
 
 
   /***************************测试程序****************************************************************************************/
@@ -142,29 +135,49 @@ void loop() {
   //     }
 
   // 蜂鸣器测试
-//  digitalWrite(Buzz, HIGH);
-//  delay(100);
-//  digitalWrite(Buzz, LOW);
+  //  digitalWrite(Buzz, HIGH);
+  //  delay(100);
+  //  digitalWrite(Buzz, LOW);
+
+  //  //一级报警
+  //  int Gsm_Vaule = digitalRead(GsmPIN);
+  ////  int Gsm_Vaule_1 = !Gsm_Vaule;
+  //  Serial.print("  GSM   ");
+  //  Serial.print(Gsm_Vaule);
+  //  Serial.print("  OVER  ");
+  ////  Serial.print("  GSM_1   ");
+  ////  Serial.print(Gsm_Vaule_1);
+  ////  Serial.print("  OVER  ");
+  //if(millis()>60000)
+  // {  if (Gsm_Vaule == 0)
+  //  { //Gsm_Pdu_Yiji();
+  ////    Forward();
+  //  digitalWrite(Buzz, HIGH);
+  //  delay(100);
+  //  digitalWrite(Buzz,  LOW);
+  //  }
+  // }
+  //
 
   /**********2018年7月26日测试电机内容*****************/
-//      if (comdata[3] == 0x07) //判断按键
-//      Back(); 
-//      else
-//      Forward();
-        
- /*********************************************************************************************************************************/
- 
+  //      if (comdata[3] == 0x07) //判断按键
+  //      Back();
+  //      else
+  //      Forward();
+
+  /*********************************************************************************************************************************/
+
 }
 
 void Serial_Receiving()
 {
-    while (Serial.available() > 0)
+  while (Serial.available() > 0)
   {
     comdata += char(Serial.read()); //一位一位的读取串口数据，串接到comedata
     delay(2);
   }
-    State = comdata[3];
-    comdata = ""; //数据清空，否则会影响下一次的数据  
+  State = comdata[3];
+  comdata = ""; //数据清空，否则会影响下一次的数据
 }
 
 /************************************模块函数******************************************/
@@ -174,12 +187,12 @@ void Serial_Receiving()
  ***************************/
 void Init_Data()
 {
-  Stop();
-  Serial.write(Temperature,5);
-  Serial.write(Humidity,5);
-  Serial.write(Electricity,5);
-  Serial.write(Electricity_1,5);
-  Serial.write(Concentration,5);
+  Stop(10);
+  Serial.write(Temperature, 5);
+  Serial.write(Humidity, 5);
+  Serial.write(Electricity, 5);
+  Serial.write(Electricity_1, 5);
+  Serial.write(Concentration, 5);
 }
 
 /***************************
@@ -187,16 +200,16 @@ void Init_Data()
 ***************************/
 void flash()
 {
-  Temperature[2] = Temp;
+  Temperature[2] = Temp;   //温度
   Humidity[2] = Hum;
-  Concentration[2] = Val;
+  Concentration[2] = Val;  //浓度
   Electricity[2] = Val_E;
   Electricity_1[2] = Val_E_1;
   delay(100);
   Serial.write(Temperature, 5);   //发送一帧温度数据
   Serial.write(Humidity, 5);      //发送一帧湿度数据
   Serial.write(Electricity, 5);   //发送一帧主电量数据
-  Serial.write(Electricity_1,5);  //发送一帧副电量数据
+  Serial.write(Electricity_1, 5); //发送一帧副电量数据
   Serial.write(Concentration, 5); //发送一帧浓度数据
 }
 
@@ -207,8 +220,8 @@ void MQ_2()
 {
   int val = analogRead(MQ_2PIN); //读取模拟脚的数据
   Val = (val / 10.24);
-//  Serial.print("val=");
-//  Serial.println(Val, DEC); //输出十进制
+  //  Serial.print("val=");
+  //  Serial.println(Val, DEC); //输出十进制
   delay(100);
 }
 
@@ -218,15 +231,15 @@ void MQ_2()
 void Dht11()
 {
   int chk = DHT11.read(DHT11PIN);                 //将读取到的值赋给chk
-  Temp = (float)DHT11.temperature;             //将温度值赋值给 result[0]
-  Hum = (float)DHT11.humidity;                 //将湿度值赋给 result[1]
-//  Serial.print("Tempeature:");                        //打印出Tempeature:
-//  Serial.println(Temp);                                     //打印温度结果
-//
-//  Serial.print("Humidity:");                            //打印出Humidity:
-//  Serial.print(Hum);                                     //打印出湿度结果
-//  Serial.println("%");                                  //打印出%
-//  delay(1000);                                       //延时一段时间
+  Temp = (float)DHT11.temperature;             //将温度值赋值给
+  Hum  = (float)DHT11.humidity;                 //将湿度值赋给
+  //  Serial.print("Tempeature:");                        //打印出Tempeature:
+  //  Serial.println(Temp);                                     //打印温度结果
+  //
+  //  Serial.print("Humidity:");                            //打印出Humidity:
+  //  Serial.print(Hum);                                     //打印出湿度结果
+  //  Serial.println("%");                                  //打印出%
+  //  delay(10);                                       //延时一段时间
 }
 
 /*****************************
@@ -237,15 +250,15 @@ void Electricity_Display()
   float val   = analogRead(Elec_1PIN);
   float val1  = (val * 5.2 / 1023 + 1.2); //将15V电压转化为5V为基准电压
   Val_E = val1 / 5.2 * 100; //电量百分比
-//  Serial.print("模拟量:");
-//  Serial.print(val);
-//  Serial.print("\t");
-//  Serial.print("主电压:");
-//  Serial.print(val1);
-//  Serial.print("\t");
-//  Serial.print("电量百分比:");
-//  Serial.println(Val_E);
-//  delay(1000);
+  //  Serial.print("模拟量:");
+  //  Serial.print(val);
+  //  Serial.print("\t");
+  //  Serial.print("主电压:");
+  //  Serial.print(val1);
+  //  Serial.print("\t");
+  //  Serial.print("电量百分比:");
+  //  Serial.println(Val_E);
+  //  delay(1000);
 }
 
 /*****************************
@@ -256,15 +269,15 @@ void Electricity_Display_1()
   float val_1   = analogRead(A1);
   float val1_1  = (val_1 * 5 / 1023 + 0.8); ////将10V电压转化为5V为基准电压
   Val_E_1 = val1_1 / 5 * 100; //电量百分比
-//  Serial.print(" 模拟量: ");
-//  Serial.print(val_1);
-//  Serial.print("\t");
-//  Serial.print(" 副电压: ");
-//  Serial.print(val1_1);
-//  Serial.print("\t");
-//  Serial.print(" 电量百分比: ");
-//  Serial.println(Val_E_1);
-//  delay(1000);
+  //  Serial.print(" 模拟量: ");
+  //  Serial.print(val_1);
+  //  Serial.print("\t");
+  //  Serial.print(" 副电压: ");
+  //  Serial.print(val1_1);
+  //  Serial.print("\t");
+  //  Serial.print(" 电量百分比: ");
+  //  Serial.println(Val_E_1);
+  //  delay(1000);
 }
 
 /****************************
@@ -290,7 +303,7 @@ void Gsm_Pdu(void)
 void GPRS_Call(void)
 {
   mySerial.println(ATDCall);
-  delay(2000); 
+  delay(2000);
 }
 
 /**********************************************
@@ -298,7 +311,7 @@ void GPRS_Call(void)
 **********************************************/
 void Gsm_Pdu_Yiji()
 {
-//  Forward();
+  //  Forward();
 }
 
 /***********************************电机驱动**************************/
@@ -314,16 +327,16 @@ void Forward()
   digitalWrite(6, LOW);
   delay(100);
 
-//    digitalWrite(9, HIGH);
-//    digitalWrite(5, HIGH);
-//    digitalWrite(6, LOW);
-//    digitalWrite(10, LOW);
-//    delay(900);
-//    digitalWrite(9, LOW);
-//    digitalWrite(5, LOW);
-//    digitalWrite(6, LOW);
-//    digitalWrite(10, LOW);
-//      delay(100);
+  //    digitalWrite(9, HIGH);
+  //    digitalWrite(5, HIGH);
+  //    digitalWrite(6, LOW);
+  //    digitalWrite(10, LOW);
+  //    delay(900);
+  //    digitalWrite(9, LOW);
+  //    digitalWrite(5, LOW);
+  //    digitalWrite(6, LOW);
+  //    digitalWrite(10, LOW);
+  //      delay(100);
 }
 
 /*****************************
@@ -331,35 +344,69 @@ void Forward()
  *****************************/
 void Back()
 {
+  //左轮
   digitalWrite(9, LOW);
   digitalWrite(10, HIGH);
+  //右轮
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
   delay(100);
-//  digitalWrite(9, LOW);
-//  digitalWrite(5, LOW);
-//  digitalWrite(6, HIGH);
-//  digitalWrite(10,HIGH);
-//  delay(300);
-//  digitalWrite(9, LOW);
-//  digitalWrite(5, LOW);
-//  digitalWrite(6, LOW);
-//  digitalWrite(10,LOW);
-//  delay(700);
+  //  digitalWrite(9, LOW);
+  //  digitalWrite(5, LOW);
+  //  digitalWrite(6, HIGH);
+  //  digitalWrite(10,HIGH);
+  //  delay(300);
+  //  digitalWrite(9, LOW);
+  //  digitalWrite(5, LOW);
+  //  digitalWrite(6, LOW);
+  //  digitalWrite(10,LOW);
+  //  delay(700);
 }
 
 /*****************************
    停止
  *****************************/
-void Stop()
+void Stop(int s2)
 {
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  delay(1000);
+  while (s2--) {
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    delay(100);
+  }
 }
 //
+
+void Turn_right(int s)
+{
+  while (s--) {
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    delay(100);
+    //  digitalWrite(9, LOW);
+    //  digitalWrite(10, LOW);
+    //  digitalWrite(5, LOW);
+    //  digitalWrite(6, LOW);
+    //  delay(10);
+
+  }
+}
+
+void Forward_count(int s1)
+{
+  while (s1--)
+  {
+    digitalWrite(9, HIGH);
+    digitalWrite(10, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    delay(100);
+  }
+}
+
 
 
 
